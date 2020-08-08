2326297430af2a14a6f56ed2fc30be9f772ea650
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-08 09:47:39 -0400

    Call the Reload function instead of simulating a key press

--- ControllerReload/Script.cs
+++ ControllerReload/Script.cs
@@ -1,13 +1,20 @@
 using GTA;
-using GTA.UI;
 using System;
-using System.Windows.Forms;
-
+using System.Reflection;
 
 namespace ControllerReload
 {
     public class ControllerReload : Script
     {
+        #region Private Fields
+
+        /// <summary>
+        /// The Method information of the Reload console command.
+        /// </summary>
+        private static MethodInfo reloadMethod = null;
+
+        #endregion
+
         /// <summary>
         /// The script configuration.
         /// </summary>
@@ -20,21 +27,56 @@ namespace ControllerReload
         /// The second control to press.
         /// </summary>
         public static GTA.Control PressTwo = Config.GetValue("ControllerReload", "PressTwo", GTA.Control.Reload);
-
-        public static string ScriptHookReloadButton = Config.GetValue("ControllerReload", "ScriptHookReloadButton", "{F10}");
         
     
         public ControllerReload()
         {
+            // Create a place to store the assembly
+            AssemblyName found = null;
+
+            // Get all of the assemblies loaded in SHVDN
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                // Get the name information
+                AssemblyName name = assembly.GetName();
+                // If is SHVDN, save it and break the iterator
+                if (name.Name == "ScriptHookVDotNet")
+                {
+                    found = name;
+                    break;
+                }
+            }
+
+            // If SHVDN was not found, raise an exception
+            if (found == null)
+            {
+                throw new InvalidOperationException("The SHVDN Assembly was not found.");
+            }
+            // Otherwise, find the class and raise an exception if it was not found
+            Type @class = Type.GetType($"ScriptHookVDotNet, {found}", false);
+            if (@class == null)
+            {
+                throw new InvalidOperationException("The ScriptHookVDotNet Class was not found.");
+            }
+            // Then, continue with the method (is the Reload console command)
+            MethodInfo method = @class.GetMethod("Reload");
+            if (method == null)
+            {
+                throw new InvalidOperationException("The Reload Function/Method was not found.");
+            }
+
+            // At this point, we have a valid method
+            // Save it on a field for later use
+            reloadMethod = method;
+            // And finish with the Tick event
             Tick += OnTick;
         }
 
         public void OnTick(object Sender, EventArgs Args)
         {
             if (Game.IsControlPressed(PressOne) == true && Game.IsControlPressed(PressTwo) == true)
-            {                              
-                SendKeys.SendWait(ScriptHookReloadButton);
-                GTA.UI.Notification.Show("Scripts Reloaded!");
+            {
+                reloadMethod.Invoke(null, new object[0]);
             }
         }
     }
