e2d16d3dd9a74f54709a5bb1b7f4729f91173b8d
Hannele Ruiz <justlemoncl@gmail.com>
2023-09-16 00:07:14 -0300

    Use new namespace format

--- ControllerReload/Script.cs
+++ ControllerReload/Script.cs
@@ -1,98 +1,97 @@
-using GTA;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using System;
 using System.IO;
 using System.Reflection;
+using GTA;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
-namespace ControllerReload
+namespace ControllerReload;
+
+public class ControllerReload : Script
 {
-    public class ControllerReload : Script
+    #region Fields
+
+    private static MethodInfo reloadMethod;
+    private static Configuration config;
+
+    #endregion
+    
+    #region Constructors
+
+    public ControllerReload()
     {
-        #region Fields
+        JsonSerializerSettings settings = new JsonSerializerSettings();
+        settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+        settings.Converters.Add(new StringEnumConverter());
+        string path = GetRelativeFilePath("ControllerReload.json");
 
-        private static MethodInfo reloadMethod;
-        private static Configuration config;
-
-        #endregion
-
-        #region Constructors
-
-        public ControllerReload()
+        if (File.Exists(path))
         {
-            JsonSerializerSettings settings = new JsonSerializerSettings();
-            settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
-            settings.Converters.Add(new StringEnumConverter());
-            string path = GetRelativeFilePath("ControllerReload.json");
-
-            if (File.Exists(path))
-            {
-                string contents = File.ReadAllText(path);
-                config = JsonConvert.DeserializeObject<Configuration>(contents, settings);
-            }
-            else
-            {
-                config = new Configuration();
-                string contents = JsonConvert.SerializeObject(config, Formatting.Indented, settings);
-                File.WriteAllText(path, contents);
-            }
-
-            AssemblyName found = null;
-
-            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                AssemblyName name = assembly.GetName();
-
-                if (name.Name == "ScriptHookVDotNet")
-                {
-                    found = name;
-                    break;
-                }
-            }
-
-            if (found == null)
-            {
-                throw new InvalidOperationException("The SHVDN Assembly was not found.");
-            }
-
-            Type @class = Type.GetType($"ScriptHookVDotNet, {found}", false);
-            if (@class == null)
-            {
-                throw new InvalidOperationException("The ScriptHookVDotNet Class was not found.");
-            }
-
-            MethodInfo method = @class.GetMethod("Reload");
-            if (method == null)
-            {
-                throw new InvalidOperationException("The Reload Function/Method was not found.");
-            }
-
-            reloadMethod = method;
-            Tick += OnTick;
+            string contents = File.ReadAllText(path);
+            config = JsonConvert.DeserializeObject<Configuration>(contents, settings);
+        }
+        else
+        {
+            config = new Configuration();
+            string contents = JsonConvert.SerializeObject(config, Formatting.Indented, settings);
+            File.WriteAllText(path, contents);
         }
 
-        #endregion
+        AssemblyName found = null;
 
-        #region Event Functions
-
-        private void OnTick(object sender, EventArgs e)
+        foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
         {
-            if (Game.LastInputMethod != InputMethod.GamePad)
+            AssemblyName name = assembly.GetName();
+
+            if (name.Name == "ScriptHookVDotNet")
+            {
+                found = name;
+                break;
+            }
+        }
+
+        if (found == null)
+        {
+            throw new InvalidOperationException("The SHVDN Assembly was not found.");
+        }
+
+        Type @class = Type.GetType($"ScriptHookVDotNet, {found}", false);
+        if (@class == null)
+        {
+            throw new InvalidOperationException("The ScriptHookVDotNet Class was not found.");
+        }
+
+        MethodInfo method = @class.GetMethod("Reload");
+        if (method == null)
+        {
+            throw new InvalidOperationException("The Reload Function/Method was not found.");
+        }
+
+        reloadMethod = method;
+        Tick += OnTick;
+    }
+
+    #endregion
+
+    #region Event Functions
+
+    private void OnTick(object sender, EventArgs e)
+    {
+        if (Game.LastInputMethod != InputMethod.GamePad)
+        {
+            return;
+        }
+
+        foreach (Control control in config.Controls)
+        {
+            if (!Game.IsControlPressed(control))
             {
                 return;
             }
-
-            foreach (Control control in config.Controls)
-            {
-                if (!Game.IsControlPressed(control))
-                {
-                    return;
-                }
-            }
-
-            reloadMethod.Invoke(null, new object[0]);
         }
 
-        #endregion
+        reloadMethod.Invoke(null, new object[0]);
     }
+
+    #endregion
 }
